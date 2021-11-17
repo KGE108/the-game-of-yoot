c4244a3c878cc5c6522091c5f1b9e9d837b419a4
aries043 <aries2_0423@naver.com>
2021-11-18 00:38:48 +0900

    Creating a function named Throwing_Yuts.c.

---
+++ Throwing_Yuts.c
@@ -0,0 +1,44 @@
+void Throwing_Yuts(int* yut_array, int *count)
+{
+	const int go[16] = { 1,1,1,2,2,2,2,2,2,3,3,3,3,4,5,6 };
+	int state = 0, i = 0;
+	yut_array[20] = { 0, };
+	while (1)
+	{
+		int yut = go[rand() % 16];
+		state = 0;
+		switch (yut)
+		{
+		case 1:
+			yut_array[i] = yut;
+			printf("도!\n");
+			break;
+		case 2:
+			yut_array[i] = yut;
+			printf("개!\n");
+			break;
+		case 3:
+			yut_array[i] = yut;
+			printf("걸!\n");
+			break;
+		case 4:
+			yut_array[i] = yut;
+			printf("윷!\n");
+			i++;
+			state = 1;
+			break;
+		case 5:
+			yut_array[i] = yut;
+			printf("모!\n");
+			i++;
+			state = 1;
+			break;
+		case 6:
+			yut_array[i] = yut;
+			printf("빽도!\n");
+			break;
+		}
+		if (state == 0) break;
+	}
+	*count = i + 1;
+}
