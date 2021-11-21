39aa5a82cd682c044df6cfdc9aafafbf95eb5759
aries043 <aries2_0423@naver.com>
2021-11-21 23:07:04 +0900

    It added a function that requires input of the enter key when throwing yut.

--- Deciding_the_Order.c
+++ Deciding_the_Order.c
@@ -2,6 +2,7 @@ void Deciding_the_Order(char* name1, char* name2)
 {
 
 	int go[16] = { 1,1,1,2,2,2,2,2,2,3,3,3,3,4,5,0 };
+	char act;
 	char name_yut1[20];
 	char name_yut2[20];
 	char zero[10] = "빽도";
@@ -11,8 +12,9 @@ void Deciding_the_Order(char* name1, char* name2)
 	char four[4] = "윷";
 	char five[4] = "모";
 Ex:
+	printf("윷을 던지시려면 엔터를 눌러주세요.\n\n");
+	scanf("%c", &act);
 	int yut1 = go[rand() % 16];
-	int yut2 = go[rand() % 16];
 	switch (yut1)
 	{
 	case 0:
@@ -34,6 +36,10 @@ Ex:
 		strcpy(name_yut1, five);
 		break;
 	}
+	printf("------------------------------\n%s의 윷: %s\n------------------------------\n\n\n", name1, name_yut1);
+	printf("윷을 던지시려면 엔터를 눌러주세요.\n\n");
+	scanf("%c", &act);
+	int yut2 = go[rand() % 16];
 	switch (yut2)
 	{
 	case 0:
@@ -55,15 +61,14 @@ Ex:
 		strcpy(name_yut2, five);
 		break;
 	}
-	printf("------------------------------\n%s의 윷: %s\n\n", name1, name_yut1);
-	printf("%s의 윷: %s\n------------------------------\n\n", name2, name_yut2);
+	printf("------------------------------\n%s의 윷: %s\n------------------------------\n\n\n", name2, name_yut2);
 	if (yut1 == yut2)
 	{
 		printf("두 팀의 윷이 같아 재진행합니다!\n\n");
 		goto Ex;
 	}
 
-	if (yut1 < yut2) printf("%s의 선입니다!\n\n", name1);
+	if (yut1 < yut2) printf("%s의 선입니다!\n\n\n", name1);
 
-	if (yut1 > yut2) printf("%s의 선입니다!\n\n", name2);
+	if (yut1 > yut2) printf("%s의 선입니다!\n\n\n", name2);
 } //main func needs "srand(time(NULL))", team names
