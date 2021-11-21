4b5086afaf81a13954a872dd56f909b735e67321
aries043 <aries2_0423@naver.com>
2021-11-21 23:05:03 +0900

    Creating a function named Throwing_Yuts.c.

---
+++ Deciding_the_Order.c
@@ -0,0 +1,69 @@
+void Deciding_the_Order(char* name1, char* name2)
+{
+
+	int go[16] = { 1,1,1,2,2,2,2,2,2,3,3,3,3,4,5,0 };
+	char name_yut1[20];
+	char name_yut2[20];
+	char zero[10] = "빽도";
+	char one[4] = "도";
+	char two[4] = "개";
+	char three[4] = "걸";
+	char four[4] = "윷";
+	char five[4] = "모";
+Ex:
+	int yut1 = go[rand() % 16];
+	int yut2 = go[rand() % 16];
+	switch (yut1)
+	{
+	case 0:
+		strcpy(name_yut1, zero);
+		break;
+	case 1:
+		strcpy(name_yut1, one);
+		break;
+	case 2:
+		strcpy(name_yut1, two);
+		break;
+	case 3:
+		strcpy(name_yut1, three);
+		break;
+	case 4:
+		strcpy(name_yut1, four);
+		break;
+	case 5:
+		strcpy(name_yut1, five);
+		break;
+	}
+	switch (yut2)
+	{
+	case 0:
+		strcpy(name_yut2, zero);
+		break;
+	case 1:
+		strcpy(name_yut2, one);
+		break;
+	case 2:
+		strcpy(name_yut2, two);
+		break;
+	case 3:
+		strcpy(name_yut2, three);
+		break;
+	case 4:
+		strcpy(name_yut2, four);
+		break;
+	case 5:
+		strcpy(name_yut2, five);
+		break;
+	}
+	printf("------------------------------\n%s의 윷: %s\n\n", name1, name_yut1);
+	printf("%s의 윷: %s\n------------------------------\n\n", name2, name_yut2);
+	if (yut1 == yut2)
+	{
+		printf("두 팀의 윷이 같아 재진행합니다!\n\n");
+		goto Ex;
+	}
+
+	if (yut1 < yut2) printf("%s의 선입니다!\n\n", name1);
+
+	if (yut1 > yut2) printf("%s의 선입니다!\n\n", name2);
+} //main func needs "srand(time(NULL))", team names
