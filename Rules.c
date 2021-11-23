fb1311fa40522db4a0961b96031bf9c93c1956a5
kge782108 <kge782108@chungbuk.ac.kr>
2021-11-23 17:41:14 +0900

    Changed the design of Rules.c

--- Rules.c
+++ Rules.c
@@ -1,27 +1,16 @@
 void Rules()
 {
 	printf("-----------\n\n윷놀이 규칙\n\n-----------\n");
-	Sleep(3000);
-	system("cls");
 	printf("-<이동 수>-\n");
 	printf("도-1칸\n개-2칸\n걸-3칸\n윷-4칸\n모-5칸\n빽도-뒤로 1칸\n\n");
 	Sleep(4000);
 	system("cls");
+	printf("-----------\n\n윷놀이 규칙\n\n-----------\n");
 	printf("1. 각 팀이 윷을 던져 작은 값이 나오는 팀이 먼저 한다.\n");
-	Sleep(4000);
-	system("cls");
 	printf("2. 윷을 던져서 윷 판을 한바퀴 먼저 도는 팀이 이긴다.\n");
-	Sleep(4000);
-	system("cls");
 	printf("3. 윷과 모가 나오면 한번씩 더 던진다.\n");
-	Sleep(4000);
-	system("cls");
 	printf("4. 가다가 다른 팀의 말 위에 겹치면 원래 있던 팀의 말이 시작지점으로 돌아간다.\n");
-	Sleep(4000);
-	system("cls");
 	printf("5. 가다가 같은 팀의 말 위에 겹치면 두 말이 같이 움직인다.\n");
-	Sleep(4000);
-	system("cls");
 	printf("6. 2점을 먼저 내는 팀이 이긴다.");
 	Sleep(4000);
 	system("cls");
